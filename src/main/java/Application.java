b47586e88bcb1a09e32747815b5f6eb99abee8a4
Lucas Vinicius Salviano Rodrigues <lucas.vinicius.rodrigues@hotmail.com>
2020-11-18 21:23:46 -0300

    Adding base code

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,25 @@
+import infrastructure.service.TelegramBotService;
+
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Application {
 
     public static void main(String[] args) {
 
-        System.out.println("Here we begin");
+        try(InputStream input = Application.class.getClassLoader().getResourceAsStream("application.properties")) {
 
+            Properties properties = new Properties();
+            properties.load(input);
+
+            TelegramBotService telegramBotRepository = new TelegramBotService(properties.getProperty("telegram-token-access"));
+
+            while (true) {
+                telegramBotRepository.exec();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
